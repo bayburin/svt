43ff8fb76a1145d9344e2217fc24cc74f8ae3408
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-02-06 15:25:26 +0700

    Many changes. Changed bootstrap styles, js, workspace controller.

--- app/controllers/workplaces_controller.rb
+++ app/controllers/workplaces_controller.rb
@@ -1,2 +1,31 @@
 class WorkplacesController < ApplicationController
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json do
+        # @workplaces = Workplace.all
+        render json: [
+          { name: 'Место-714-1', type: 'Конструкторское', responsible: 'Байбурин Р.Ф.', location: '3а-321а', count: 4},
+          { name: 'Место-714-2', type: 'Офисное', responsible: 'Байбурин Р.Ф.', location: '3а-321а', count: 2}
+        ]
+      end
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
