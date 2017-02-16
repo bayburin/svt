88f8757462d8690b97de1e986c28b351954beec4
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-02-16 11:22:29 +0700

    Added models and controllers.

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,14 @@ module Inv
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Krasnoyarsk'
     config.i18n.default_locale = :ru
+
+    # Provides support for Cross-Origin Resource Sharing (CORS)
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://lk-3-dev', 'https://lk-test-3.iss-reshetnev.ru'
+        resource '/lk_invents/init/*', :headers => :any, :methods => [:get]
+        resource '/lk_invents/show_division_data/*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
