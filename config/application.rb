50b90b6132cfe9b23c504e28e6b3bf632534918d
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-04-13 16:59:42 +0700

    Chaned application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,8 @@ module Inv
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins   'http://lk-3-dev', 'https://lk-test-3.iss-reshetnev.ru', 'https://lk-test-3.npopm.ru',
-                  'http://lk-test-3.iss-reshetnev.ru', 'http://lk-test-3.npopm.ru'
+                  'http://lk-test-3.iss-reshetnev.ru', 'http://lk-test-3.npopm.ru', 'lk-test-3.npopm.ru',
+                  'lk-test-3.iss-reshetnev.ru'
         resource  '/inventory/lk_invents/init/*', :headers => :any, :methods => [:get]
         resource  '/inventory/lk_invents/show_division_data/*', :headers => :any, :methods => [:get]
         resource  '/inventory/lk_invents/get_data_from_audit/*', :headers => :any, :methods => [:get]
