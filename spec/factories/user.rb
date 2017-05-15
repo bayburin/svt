7b302d6ebaf716f671fe1fe925540ad18f4a3fac
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-05-15 14:09:58 +0700

    Feature Sign_in_spec.rb. Add webkit driver.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    tn 17_664
-    info 'Байбурин Равиль Фаильевич'
+    tn 101_101
+    fullname 'Тест Личного кабинета'
+    password 'xxxx1234'
     association :role, factory: :admin_role
   end
 
