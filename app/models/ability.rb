53966a6e2aab1e49d3eefeae4fdec35e4dcf626f
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-05-29 15:31:59 +0700

    Interim commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,10 @@ class Ability
       can :manage, :all
     elsif user.has_role? :lk_user
       can :manage, :lk_invent
+      # can :init_properties, :lk_invent
+      # can :show_division_data, :lk_invent, {  }
+      # can :pc_config_from_audit, :lk_invent
+      # can :create_workplace, :lk_invent
     else
       cannot :manage, :all
     end
