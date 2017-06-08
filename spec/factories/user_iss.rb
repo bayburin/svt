618dad3c079f854334e6c04e7968a42acaf3b81d
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-06-08 10:14:00 +0700

    Add bayburin_user_iss factory

--- spec/factories/user_iss.rb
+++ spec/factories/user_iss.rb
@@ -18,4 +18,24 @@ FactoryGirl.define do
     dept_kadr 0
     decret false
   end
+
+  factory :bayburin_user_iss, class: UserIss do
+    tn 17_664
+    dept 714
+    fio 'Байбурин Равиль Фаильевич'
+    room '3а-321а'
+    tel '50-32'
+    wname ''
+    email ''
+    comment ''
+    duty 'инженер-программист 3 категории'
+    status 'changed'
+    datereg 5.years.ago
+    duty_code 0
+    fio_initials 'Байбурин Р.Ф.'
+    category 4
+    id_tn 12880
+    dept_kadr 714
+    decret false
+  end
 end
