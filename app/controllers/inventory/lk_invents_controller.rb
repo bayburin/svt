d90688c07578ee9103e4e51e9a488b86ec2755a8
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-06-29 17:13:31 +0700

    Add the Pundir exception call into InitProperties service and LkInventsController

--- app/controllers/inventory/lk_invents_controller.rb
+++ app/controllers/inventory/lk_invents_controller.rb
@@ -15,7 +15,11 @@ module Inventory
       if @properties.run
         render json: @properties.data
       else
-        render json: { full_message: 'Обратитесь к администратору, т.50-32' }, status: 422
+        if @properties.data[:divisions].nil?
+          raise Pundit::NotAuthorizedError, 'Access denied'
+        else
+          render json: { full_message: 'Обратитесь к администратору, т.50-32' }, status: 422
+        end
       end
     end
 
