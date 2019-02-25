457e3a62d94d3f88366f9dbcaef3fe47d4a0ca68
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-02-25 17:35:12 +0700

    Добавил кнопку печати списка РМ для отдела на странице РМ

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :pty, true
 
 # Repo details
 set :rbenv_ruby, '2.3.1'
-set :repo_url, '/var/repos/inv.git'
+set :repo_url, 'git@gitlab.iss-reshetnev.ru:714/7141/svt.git'
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 
 set :keep_releases, 5
