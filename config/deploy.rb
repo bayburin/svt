3ee5c96e6371e65a693ed2cf3dbf93d86a10fc3c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2019-12-11 14:50:04 +0700

    Изменена версия руби в конфиге deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ server 'dc', user: 'deployer', roles: %w[web app db]
 set :pty, true
 
 # Repo details
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.5'
 set :repo_url, 'git@gitlab.iss-reshetnev.ru:714/7141/svt.git'
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 
