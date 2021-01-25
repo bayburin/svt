950f720a1d6ef2891272cb3cf579678597e82e55
Lika <bartuzanovaan@iss-reshetnev.ru>
2021-01-25 17:00:16 +0700

    Переделано получение данных о конфигурации техники из бд Audit в вызов rest api

--- spec/factories/host_iss.rb
+++ spec/factories/host_iss.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :host_iss, class: Hash do
     # Здесь показаны не все параметры таблицы 'hosts'.
-    id { '764196' }
-    ip { '10.1.8.26' }
-    tn { 17_664 }
-    user { 'Байбурин Равиль Фаильевич' }
+    id { '768707' }
+    ip { '10.1.8.50' }
+    mac { '9c5c8e7a6fa1' }
+    tn { 3737 }
+    user { 'Блинова Ирина Рудольфовна' }
     division { '714' }
-    flag_set { 'inet_access' }
+    flag_set { 'inet_access,ad_member,auto_auth' }
 
     initialize_with { attributes }
   end
